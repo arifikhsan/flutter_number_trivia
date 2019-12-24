fc88d30e12890bf754a3932e68d9d78dc8d268e5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-24 23:37:55 +0700

    add abstract classes

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_number_trivia/features/core/error/failures.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
