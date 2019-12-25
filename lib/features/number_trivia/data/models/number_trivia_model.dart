f86dd7db4ec3c27ecca3b332fd283411f2896b7f
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-25 16:39:44 +0700

    number trivia model

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+}
