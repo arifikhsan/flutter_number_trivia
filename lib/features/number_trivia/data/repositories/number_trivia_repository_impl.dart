0d58680b01799148e75cc403eedabb8c4846fb92
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-25 17:30:08 +0700

    data repository

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_number_trivia/features/core/error/failures.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    return null;
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    return null;
+  }
+}
