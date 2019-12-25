2c206c967dcf913adcc7ca174d86ce308894c00c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-25 17:46:30 +0700

    repository implementation

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,9 +1,22 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_number_trivia/features/core/error/failures.dart';
+import 'package:flutter_number_trivia/features/core/platform/network_info.dart';
+import 'package:flutter_number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {@required this.remoteDataSource,
+      @required this.localDataSource,
+      @required this.networkInfo});
+
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
     // TODO: implement getConcreteNumberTrivia
