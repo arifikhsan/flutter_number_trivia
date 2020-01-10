177930167ce89dd85824062bc1ce5b70f20c9188
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-10 09:40:59 +0700

    ,

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -5,6 +5,8 @@ class NumberTrivia extends Equatable {
   final String text;
   final int number;
 
-  NumberTrivia({@required this.text, @required this.number})
-      : super([text, number]);
+  NumberTrivia({
+    @required this.text,
+    @required this.number,
+  }) : super([text, number]);
 }
