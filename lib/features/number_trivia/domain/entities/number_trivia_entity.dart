cbf48802f2d397adff56e4bfd1b7469caf522653
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-11 08:11:31 +0700

    change number trivia to number trivia entity

--- lib/features/number_trivia/domain/entities/number_trivia_entity.dart
+++ lib/features/number_trivia/domain/entities/number_trivia_entity.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-class NumberTrivia extends Equatable {
+class NumberTriviaEntity extends Equatable {
   final String text;
   final int number;
 
-  NumberTrivia({
+  NumberTriviaEntity({
     @required this.text,
     @required this.number,
   }) : super([text, number]);
